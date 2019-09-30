60558f53bfa6c1386914911dae23951c600ded51
luyinxing <7237902@qq.com>
2019-09-30 12:10:04 +0800

    代码优化

--- src/main/java/com/geostar/geosmarter/wordcount/RemoveMaxOccurrenceWordMain.java
+++ src/main/java/com/geostar/geosmarter/wordcount/RemoveMaxOccurrenceWordMain.java
@@ -39,90 +39,78 @@ public class RemoveMaxOccurrenceWordMain implements Serializable {
 		conf.setMaster(Common.MASTER_NAME).setAppName("Remove More Occurrence Word");
 		JavaSparkContext jsc = new JavaSparkContext(conf);
 
-		//Step 2:读取数据
-		JavaRDD<String> lines = loadFile(jsc);
+		//Step 2:读取数据，生成第一个RDD
+		JavaRDD<String> lines = jsc.textFile("RunJar/src/main/resources/remove_max_occurrence_test_data.txt");
 
-		//Step 3:RDD转换
-		// 获取一个一个的单词
+		//Step 3:flatMap操作，将每行数据以空格切分，并扁平化处理
 		JavaRDD<String> wordsRDD = lines.flatMap(new FlatMapFunction<String, String>() {
 			private static final long serialVersionUID = 1L;
 			@Override
-			public Iterator<String> call(String line) throws Exception {
+			public Iterator<String> call(String line) {
 				return Arrays.asList(line.split(Common.BLANK)).iterator();
 			}
 		});
 
-		// 给每个单词赋值1，返回<word, 1>形式
+		//Step 4:mapToPair操作，将String转为String，Integer类型，每个单词计数为1
 		JavaPairRDD<String, Integer> wordWithValueRDD = wordsRDD.mapToPair(new PairFunction<String, String, Integer>() {
 			private static final long serialVersionUID = 1L;
 			@Override
-			public Tuple2<String, Integer> call(String word) throws Exception {
-				return new Tuple2<String, Integer>(word, 1);
+			public Tuple2<String, Integer> call(String word) {
+				return new Tuple2<>(word, 1);
 			}
 		});
 
-		// 对每个单词进行统计
+		//Step 5:reduceByKey操作，相同的key将其value相加
 		JavaPairRDD<String, Integer> wordWithTotalNumRDD = wordWithValueRDD.reduceByKey(new Function2<Integer, Integer, Integer>() {
 			private static final long serialVersionUID = 1L;
 			@Override
-			public Integer call(Integer v1, Integer v2) throws Exception {
+			public Integer call(Integer v1, Integer v2) {
 				return v1 + v2;
 			}
 		});
 
-		// 把<word, totalNum>形式，交换位置为<totalNum, word>
+		//Step 6:mapToPair操作，将key与value互换位置
 		JavaPairRDD<Integer, String> totalNumAndWordRDD = wordWithTotalNumRDD.mapToPair(new PairFunction<Tuple2<String, Integer>, Integer, String>() {
 			private static final long serialVersionUID = 1L;
 			@Override
-			public Tuple2<Integer, String> call(Tuple2<String, Integer> tuple2) throws Exception {
+			public Tuple2<Integer, String> call(Tuple2<String, Integer> tuple2) {
 				return tuple2.swap();
 			}
 		});
 
-		// 对<totalNum, word>形式进行降序排序
+		//Step 7:sortByKey操作，默认正序true
 		JavaPairRDD<Integer, String> totalNumAndWordSortByKeyRDD = totalNumAndWordRDD.sortByKey(false);
 
-		// 这里把排序好的结果放入Cache，方便后面的计算
+		//Step 8:RDD缓存
 		JavaPairRDD<Integer, String> totalNumAndWordSortByKeyCachedRDD = totalNumAndWordSortByKeyRDD.cache();
 
-		// 获取第一个，即出现次数最多的那个单词
+		//Step 9:取出第一个值，即出现最多的单词
 		List<Tuple2<Integer, String>> takeOne = totalNumAndWordSortByKeyCachedRDD.take(1);
-
-		// 这个是就是出现次数最多的单词
 		final String oneWord = takeOne.get(0)._2;
 		Integer oneWordTotalNum = takeOne.get(0)._1;
-
-		/**
-		 * output: 
-		 * Find the Max Occurrence Word : a, Number : 6
-		 */
 		logger.info("Find the Max Occurrence Word : " + oneWord + ", Number : " + oneWordTotalNum);
 
-		// 我们之前对所有单词以及他们出现的次数进行了降序排序
-		// 那么，我们现在只需要把结果中，移除掉出现次数最多的单词即可
+		//Step 10：过滤，去掉出现次数最多的单子
 		JavaPairRDD<Integer, String> resultRDD = totalNumAndWordSortByKeyCachedRDD.filter(new Function<Tuple2<Integer, String>, Boolean>() {
 			private static final long serialVersionUID = 1L;
 			@Override
-			public Boolean call(Tuple2<Integer, String> tuple2) throws Exception {
+			public Boolean call(Tuple2<Integer, String> tuple2) {
 				// 移除掉出现次数最多的单词
 				return !tuple2._2.equals(oneWord);
 			}
 		});
 
-		// 打印结果
+		//Step 11:将key和value互换并输出
 		resultRDD.foreach(new VoidFunction<Tuple2<Integer, String>>() {
 			private static final long serialVersionUID = 1L;
 			@Override
-			public void call(Tuple2<Integer, String> tuple2) throws Exception {
+			public void call(Tuple2<Integer, String> tuple2) {
 				logger.info(tuple2.swap());
 			}
 		});
 
+		//Step 12:关闭会话
 		jsc.stop();
 	}
 
-	private static JavaRDD<String> loadFile(JavaSparkContext jsc) {
-		JavaRDD<String> lines = jsc.textFile("src/main/resources/remove_max_occurrence_test_data.txt");
-		return lines;
-	}
 }
